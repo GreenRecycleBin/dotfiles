d20e42d1d50608b3512dd9eeb66c9556c1e16928
Daniel Le <greenrecyclebin@gmail.com>
2015-10-18 17:12:19 +0800

    Update refactor-nrepl to 2.0.0-SNAPSHOT

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]
                   [lein-autoexpect "1.6.0"]
-                  [refactor-nrepl "1.2.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.0.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.10"]]}}
