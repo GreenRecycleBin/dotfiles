943488742c3b5c7911c8895f9c29e426d73e35af
Daniel Le <greenrecyclebin@gmail.com>
2016-04-10 13:32:40 +0800

    Remove cider-nrepl and tools.nrepl configurations

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,4 +1,2 @@
-{:user {:plugins [[cider/cider-nrepl "0.10.0"]
-                  [lein-autoexpect "1.7.0"]
-                  [refactor-nrepl "1.1.0"]]
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]]}}
+{:user {:plugins [[lein-autoexpect "1.7.0"]
+                  [refactor-nrepl "2.0.0"]]}}
