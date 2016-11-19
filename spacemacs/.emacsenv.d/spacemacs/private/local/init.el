ca5ca2974873830d76914839becad1c2aac00551
Daniel Le <greenrecyclebin@gmail.com>
2016-11-19 20:18:14 +0800

    Enable Spacemacs YAML layer

--- spacemacs/.emacsenv.d/spacemacs/private/local/init.el
+++ spacemacs/.emacsenv.d/spacemacs/private/local/init.el
@@ -39,6 +39,7 @@ values."
      ;; spell-checking
      ;; syntax-checking
      version-control-enhancements
+     yaml
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
